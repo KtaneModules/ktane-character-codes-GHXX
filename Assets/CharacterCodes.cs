04e18ef89e6a886b1b3e01e15c4ff7760b44ab58
GHXX <GHXX@gmx.at>
2019-06-27 01:21:24 +0200

    renamed a few things, cleanup

--- Assets/CharacterCodes.cs
+++ Assets/CharacterCodes.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class TheDealmakerScript : MonoBehaviour
+public class CharacterCodes : MonoBehaviour
 {
 
     public KMAudio Audio;
